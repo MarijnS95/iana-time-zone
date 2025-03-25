89b3879d906c81dd898101aa1ff97634128acbdc
Marijn Suijten <marijns95@gmail.com>
2025-03-25 15:53:26 +0100

    windows-bindgen 0.64

--- api_gen/src/main.rs
+++ api_gen/src/main.rs
@@ -1,5 +1,8 @@
 use windows_bindgen::bindgen;
 
 fn main() {
-    bindgen(["--etc", "bindings.txt"])
+    // Intentionally ignore warnings.  We only need the GetTimeZone method on Calendar, but bindgen
+    // will complain that it can't (fully) generate a few other methods because of lacking other
+    // types.  We're not interested in generating (more bloated / unused) bindings for those.
+    let _ = bindgen(["--etc", "bindings.txt"]);
 }
